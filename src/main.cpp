8848014bceb9e830a40faae22a9088a2fa1f4256
x0r4ks <max1500demo@gmail.com>
2024-10-16 17:52:40 +0300

    usage example

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,8 @@ int main(int argc, char** argv)
     // cv::imshow(path, img);
     // cv::waitKey(0);
 
-    po::options_description desc("Usage");
+    po::options_description desc("Usage: \n\timcrypto -e -s <path_to_img> -o [path_to_out_image=out.*] -t <path_to_text_file> -k <crypt key>\n\
+                                           \timcrypto -d -s <path_to_img> -o [path_to_out_text_file=out.txt] -k <crypt key>");
 
     try
     {
